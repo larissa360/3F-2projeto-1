7d14a26a770f457154c0f668ccf49102b48e8ff5
afonso cavichioli <108934804+jkprivxx@users.noreply.github.com>
2024-09-27 08:53:18 -0300

    Update informacaoGlobais.js

--- informacaoGlobais.js
+++ informacaoGlobais.js
@@ -1,3 +1,13 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
 
-console.log(url);
+async function visualizaDadosGlobais(){
+    const res = await fetch(url);
+    const dados = await res.json();
+    console.log(dados);
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-textos')
+    const container = document.getElementById('graficos-container')
+}
+
+visualizaDadosGlobais();
