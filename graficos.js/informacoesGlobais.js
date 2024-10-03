1f5041e8675f3cbf21ccd46da3729073be305763
afonso cavichioli <108934804+jkprivxx@users.noreply.github.com>
2024-10-03 09:45:31 -0300

    Update informacoesGlobais.js

--- graficos.js/informacoesGlobais.js
+++ graficos.js/informacoesGlobais.js
@@ -3,11 +3,16 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizaDadosGlobais(){
     const res = await fetch(url);
     const dados = await res.json();
+    const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
+    const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
+    const tempoMedio = dados.tempo_medio;
     console.log(dados);
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-textos')
     const container = document.getElementById('graficos-container')
+    paragrafo.innerHTML = `No mundo tem ${totalPessoasMundo} bilhõesde de habitantes, dos quais ${totalPessoasConectadas} bolhões estão conectados em alguma rede social por um tempo médio de ${tempoMedio}.`
+    container.appendChild(paragrafo);
 }
 
 visualizaDadosGlobais();
